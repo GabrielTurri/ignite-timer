8a77228208708ba2d1586cdb5a42ee6594dfcaf8
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-10-14 23:06:53 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from './components/Button'
+
 export function App() {
   return (
-   <h1>Ignite Timer</h1>
+   <>
+    <Button variant='primary' />
+    <Button variant='secondary' />
+    <Button variant='danger' />
+    <Button variant='success' />
+   </>
   )
 }
 
